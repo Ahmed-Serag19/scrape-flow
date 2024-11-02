30e55d2188645b20f05f903b8f8e751650e681d3
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-11-02 16:44:45 +0200

    finished sidebar with its icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,10 @@ import {
   ShieldCheckIcon,
 } from "lucide-react";
 import React from "react";
+import Logo from "./Logo";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 const routes = [
   {
@@ -31,9 +35,34 @@ const routes = [
 ];
 
 const DesktopSidebar = () => {
+  const pathname = usePathname();
+  const activeRoute =
+    routes.find(
+      (route) => route.href.length > 0 && pathname.includes(route.href)
+    ) || routes[0];
   return (
     <div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
-      <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4"></div>
+      <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
+        <Logo />
+      </div>
+      <div className="p-2">TODO CREDITS</div>
+      <nav className="flex flex-col p-2">
+        {routes.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={buttonVariants({
+              variant:
+                activeRoute.href === route.href
+                  ? "sidebarActiveItem"
+                  : "sidebarItem",
+            })}
+          >
+            <route.icon size={20} />
+            {route.label}
+          </Link>
+        ))}
+      </nav>
     </div>
   );
 };
